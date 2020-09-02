81acfac2c8c54486508272c7191ecaf66ae33018
Moises Sanabria <sesmoi.data@gmail.com>
2020-09-02 09:11:51 -0400

    initialised Story component, props image profileSrc, title

--- src/components/Story/index.js
+++ src/components/Story/index.js
@@ -1,12 +1,18 @@
 import * as React from 'react'
 import PropTypes from 'prop-types'
+import { Avatar } from '@material-ui/core'
 
-export default function Story () {
+export default function Story ({ image, profileSrc, title }) {
   return (
-    <div>Story</div>
+    <div className='story'>
+      <Avatar src={profileSrc} />
+      <h4>{title}</h4>
+    </div>
   )
 }
 
 Story.propTypes = {
-
+  image: PropTypes.node.isRequired,
+  profileSrc: PropTypes.string.isRequired,
+  title: PropTypes.title.string.isRequired
 }
