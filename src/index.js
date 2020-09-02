4063d29c6860cfda6e7c8c7528ab81003c70ad07
Moises Sanabria <sesmoi.data@gmail.com>
2020-09-02 09:00:33 -0400

    importing Header, Sidebar, Feed, Widgets functional components

--- src/index.js
+++ src/index.js
@@ -2,15 +2,29 @@ import * as React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Header from './Header'
+import Sidebar from './Sidebar'
+import Feed from './Feed'
+import Widgets from './Widgets'
 
 // Component
 // State
 // Lifecycle
 // UI
 
+// Your UI is a function of your state
+
 function App () {
   return (
-    <h1>Welcome to Facefoo!</h1>
+    <div className='app'>
+      <Header />
+
+      <div className='app-body'>
+        <Sidebar />
+        <Feed />
+        <Widgets />
+      </div>
+    </div>
   )
 }
 
