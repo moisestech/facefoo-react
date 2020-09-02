8876665f9f1a7226428392f0053d29f9e16b5438
Moises Sanabria <sesmoi.data@gmail.com>
2020-09-02 09:09:46 -0400

    initialised Header component with notes for SearchBar, TopHeaderMenu, ProfileRightHandSide

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,16 @@
 import * as React from 'react'
 import PropTypes from 'prop-types'
 
+// rfce
+
 export default function Header () {
   return (
-    <h1>Header</h1>
+    <div>
+      <div>Header</div>
+      {/* Search Bar */}
+      {/* Top Header Menu */}
+      {/* Profile Right Hand Side */}
+    </div>
   )
 }
 
