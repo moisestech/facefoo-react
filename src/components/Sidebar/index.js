6eceabd3fa525f81ad73933824be8462cee66149
Moises Sanabria <sesmoi.data@gmail.com>
2020-10-28 10:21:13 -0400

    icons, StateProvider useStateValue, useStateValue user dispatch, SidebarRow phtoURL displayName, icons

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,12 +1,35 @@
-import * as React from 'react'
-import PropTypes from 'prop-types'
+import "./sidebar.css";
 
-export default function Sidebar () {
+// COMPONENT
+import SidebarRow from "./components/SidebarRow";
+
+// ICONS
+import { ExpandMoreOutlined } from "@material-ui/icons";
+import LocalHospitalIcon from "@material-ui/icons/LocalHospital";
+import EmojiFlagsIcon from "@material-ui/icons/EmojiFlags";
+import PeopleIcon from "@material-ui/icons/People";
+import ChatIcon from "@material-ui/icons/Chat";
+import StorefrontIcon from "@material-ui/icons/Storefront";
+import VideoLibraryIcon from "@material-ui/icons/VideoLibrary";
+
+// REDUX
+import { useStateValue } from "../../state/StateProvider";
+
+export default function Sidebar() {
+  const [{ user }, dispatch] = useStateValue();
   return (
-    <div>Sidebar</div>
-  )
-}
-
-Sidebar.propTypes = {
-
+    <div className="sidebar">
+      <SidebarRow src={useStateValue.photoURL} title={user.displayName} />
+      <SidebarRow
+        Icon={LocalHospitalIcon}
+        title="COVID-19 Information Center"
+      />
+      <SidebarRow Icon={EmojiFlagsIcon} title="Pages" />
+      <SidebarRow Icon={PeopleIcon} title="Friends" />
+      <SidebarRow src={ChatIcon} title="Messenger" />
+      <SidebarRow src={StorefrontIcon} title="Marketplace" />
+      <SidebarRow src={VideoLibraryIcon} title="Videos" />
+      <SidebarRow src={ExpandMoreOutlined} title="More" />
+    </div>
+  );
 }
