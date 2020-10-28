7495aad4b503155d05fcfdb2dab66e8ad49fbfd0
Moises Sanabria <sesmoi.data@gmail.com>
2020-10-28 10:21:59 -0400

    widgets iframe plugins

--- src/components/Widgets/index.js
+++ src/components/Widgets/index.js
@@ -1,22 +1,18 @@
-import * as React from 'react'
-import PropTypes from 'prop-types'
+import "./widgets.css";
 
-export default function Widgets () {
+export default function Widgets() {
   return (
-    <div className='widgets'>
+    <div className="widgets">
       <iframe
-        width='340'
-        height='100%'
-        style{{ border: 'none', overflow: 'hidden' }}
-        scrolling='no'
-        frameborder='0'
-        allowTransparency='true'
-        allow='encrypted-media'
+        src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FCodingTreeFoundation&tabs=timeline&width=340&height=1500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
+        width="340"
+        height="100%"
+        style={{ border: "none", overflow: "hidden" }}
+        scrolling="no"
+        frameborder="0"
+        allowTransparency="true"
+        allow="encrypted-media"
       />
     </div>
-  )
-}
-
-Widgets.propTypes = {
-
+  );
 }
