9d5ef46d962a8018e85829ec4ab27bd0504bf214
Moises Sanabria <sesmoi.data@gmail.com>
2020-10-28 10:22:43 -0400

    App function, imp Home NotFound, Router App

--- src/index.js
+++ src/index.js
@@ -1,34 +1,32 @@
-import * as React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import Header from './Header'
-import Sidebar from './Sidebar'
-import Feed from './Feed'
-import Widgets from './Widgets'
+import * as React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+// PAGE
+import Home from "./Pages/Home";
+import NotFound from "./Pages/NotFound";
 
 // Component
 // State
 // Lifecycle
 // UI
 
-// Your UI is a function of your state
-
-function App () {
+function App() {
   return (
-    <div className='app'>
-      <Header />
-
-      <div className='app-body'>
-        <Sidebar />
-        <Feed />
-        <Widgets />
-      </div>
-    </div>
-  )
+    <Switch>
+      <Route path="/">
+        <Home />
+      </Route>
+      <Route path="*">
+        <NotFound />
+      </Route>
+    </Switch>
+  );
 }
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('app')
-)
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("app")
+);
