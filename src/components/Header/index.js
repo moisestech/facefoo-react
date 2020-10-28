3cd8a5b0af0d424015da43c1c82712bb2e443ffa
Moises Sanabria <sesmoi.data@gmail.com>
2020-10-28 08:07:41 -0400

    imp header.css import material-ui icons, useStateValue {user}, dispatch, facebook-logo, IconButton

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,19 +1,82 @@
-import * as React from 'react'
-import PropTypes from 'prop-types'
+import "./header.css";
 
-// rfce
+// ICONS
+import SearchIcon from "@material-ui/icons/Search";
+import HomeIcon from "@material-ui/icons/Home";
+import FlagIcon from "@material-ui/icons/Flag";
+import SubscriptionsOutlinedIcon from "@material-ui/icons/SubscriptionsOutlined";
+import StorefrontOutlinedIcon from "@material-ui/icons/StorefrontOutlined";
+import SupervisedUserCirceIcon from "@material-ui/icons/SupervisedUserCircle";
+import { Avatar, IconButton } from "@material-ui/core";
+import AddIcon from "@material-ui/icons/Add";
+import ForumIcon from "@material-ui/icons/Forum";
+import NotificationsActiveIcon from "@material-ui/icons/NotificationsActive";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
-export default function Header () {
+// REDUX
+import { useStateValue } from "../../state/StateProvider";
+export default function Header() {
+  const [{ user }, dispatch] = useStateValue();
+
   return (
-    <div>
-      <div>Header</div>
-      {/* Search Bar */}
-      {/* Top Header Menu */}
-      {/* Profile Right Hand Side */}
+    <div className="header">
+      <div className="header-left">
+        <img
+          src="https://raw.githubusercontent.com/moisestech/facefoo-react/master/public/Facefoo_logo.png"
+          alt=""
+        />
+        <div className="header-input">
+          <SearchIcon />
+          <input placeholder="Search Facebook" type="text" />
+        </div>
+
+        <div className="header-center">
+          <div className="header-option header-option--active">
+            <HomeIcon fontSize="large" />
+          </div>
+
+          <div className="header-option">
+            <FlagIcon fontSize="large" />
+          </div>
+
+          <div className="header-option">
+            <SubscriptionsOutlinedIcon fontSize="large" />
+          </div>
+
+          <div className="header-option">
+            <StorefrontOutlinedIcon font-Size="large" />
+          </div>
+
+          <div className="header-option">
+            <SupervisedUserCirceIcon fontSize="large" />
+          </div>
+        </div>
+
+        <div className="header-right">
+          <div className="header-info">
+            <Avatar src={user.photoURL} />
+            <h4>{user.displayName}</h4>
+          </div>
+
+          <IconButton>
+            <AddIcon />
+          </IconButton>
+
+          <IconButton>
+            <ForumIcon />
+          </IconButton>
+
+          <IconButton>
+            <NotificationsActiveIcon />
+          </IconButton>
+
+          <IconButton>
+            <ExpandMoreIcon />
+          </IconButton>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-Header.propTypes = {
-
-}
+Header.propTypes = {};
