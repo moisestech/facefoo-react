d4e83beff33a0dbf9b198e102904591f4d284480
Moises Sanabria <sesmoi.data@gmail.com>
2020-10-29 05:26:11 -0400

    imp css, img const, className story-reel map over Story comp

---
+++ src/components/Feed/components/StoryReel/index.js
@@ -0,0 +1,35 @@
+import "./story-reel.css";
+
+const img =
+  "https://bi.im-g.pl/im/0b/a2/18/z25829387V,las--zdjecie-ilustracyjne---Od-3-kwietnia-obowiazu.jpg";
+export default function StoryReel() {
+  return (
+    <div className="story-reel">
+      <Story
+        image={img}
+        profileSrc="https://avatars3.githubusercontent.com/u/30196405?s=460&u=6bd3c8280b827a0ea3f661fc7c0c65117b19bc61&v=4"
+        title="Goon Deve3"
+      />
+      <Story
+        image={img}
+        profileSrc="https://avatars3.githubusercontent.com/u/30196405?s=460&u=6bd3c8280b827a0ea3f661fc7c0c65117b19bc61&v=4"
+        title="Goon Deve3"
+      />
+      <Story
+        image={img}
+        profileSrc="https://avatars3.githubusercontent.com/u/30196405?s=460&u=6bd3c8280b827a0ea3f661fc7c0c65117b19bc61&v=4"
+        title="Goon Deve3"
+      />
+      <Story
+        image={img}
+        profileSrc="https://avatars3.githubusercontent.com/u/30196405?s=460&u=6bd3c8280b827a0ea3f661fc7c0c65117b19bc61&v=4"
+        title="Goon Deve3"
+      />
+      <Story
+        image={img}
+        profileSrc="https://avatars3.githubusercontent.com/u/30196405?s=460&u=6bd3c8280b827a0ea3f661fc7c0c65117b19bc61&v=4"
+        title="Goon Deve3"
+      />
+    </div>
+  );
+}
