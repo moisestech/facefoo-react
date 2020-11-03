c0a97fd9a77bdbcb26994433bb1a50259f24830f
Moises Sanabria <sesmoi.data@gmail.com>
2020-11-03 06:50:35 -0500

    commented useStateValue .photoURL .displayName comp SidebarRow to isolate firebase data flow

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -11,25 +11,30 @@ import PeopleIcon from "@material-ui/icons/People";
 import ChatIcon from "@material-ui/icons/Chat";
 import StorefrontIcon from "@material-ui/icons/Storefront";
 import VideoLibraryIcon from "@material-ui/icons/VideoLibrary";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
 // REDUX
-import { useStateValue } from "../../state/StateProvider";
+// import { useStateValue } from "../../state/StateProvider";
 
 export default function Sidebar() {
-  const [{ user }, dispatch] = useStateValue();
+  // const [{ user }, dispatch] = useStateValue();
   return (
     <div className="sidebar">
-      <SidebarRow src={useStateValue.photoURL} title={user.displayName} />
+      {/* <SidebarRow src={useStateValue.photoURL} title={user.displayName} /> */}
+      <SidebarRow
+        src="https://avatars2.githubusercontent.com/u/2205020?s=400&u=9e0e505d15c8bca953fbcc3170f65b22dc08e607&v=4"
+        title="Moises Sanabria"
+      />
       <SidebarRow
         Icon={LocalHospitalIcon}
         title="COVID-19 Information Center"
       />
       <SidebarRow Icon={EmojiFlagsIcon} title="Pages" />
       <SidebarRow Icon={PeopleIcon} title="Friends" />
-      <SidebarRow src={ChatIcon} title="Messenger" />
-      <SidebarRow src={StorefrontIcon} title="Marketplace" />
-      <SidebarRow src={VideoLibraryIcon} title="Videos" />
-      <SidebarRow src={ExpandMoreOutlined} title="More" />
+      <SidebarRow Icon={ChatIcon} title="Messenger" />
+      <SidebarRow Icon={StorefrontIcon} title="Marketplace" />
+      <SidebarRow Icon={VideoLibraryIcon} title="Videos" />
+      <SidebarRow Icon={ExpandMoreIcon} title="More" />
     </div>
   );
 }
